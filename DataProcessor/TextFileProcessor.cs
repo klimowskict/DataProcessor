abb561fa64ee82348ea22bbfb04019c12418af24
Colin Klimowski <klimowskict@vcu.edu>
2020-08-12 10:32:56 -0700

    Initial Commit. Beginning Module 3.

---
+++ DataProcessor/TextFileProcessor.cs
@@ -0,0 +1,21 @@
+using System.IO;
+
+namespace DataProcessor
+{
+    public class TextFileProcessor
+    {
+        public string InputFilePath { get; }
+        public string OutputFilePath { get; }
+
+        public TextFileProcessor(string inputFilePath, string outputFilePath)
+        {
+            InputFilePath = inputFilePath;
+            OutputFilePath = outputFilePath;
+        }
+
+        public void Process()
+        {
+
+        }
+    }
+}
